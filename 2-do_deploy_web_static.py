8d5a80bef2d1d357448ec06248c584afb7e6ab67
hany0ageeb <hany0ageeb@gmail.com>
2024-03-06 13:27:42 -0800

    Task 2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@
 from fabric.api import *
 
 
-env.hosts = ['100.26.241.78', '100.26.215.50']
+env.hosts = ['100.26.215.50', '100.26.241.78']
 env.user = 'ubuntu'
 
 
@@ -20,19 +20,27 @@ def do_deploy(archive_path):
         return False
     result = put(archive_path, "/tmp/")
     remote_archive = "/tmp/" + os.path.basename(archive_path)
-    if result.failed:
-        return False
+    ret_val = result.succeeded
     dest_dir = "/data/web_static/releases/{}/".format(
         archive_path[:archive_path.rindex('.')])
-    result = run("mkdir -p {}".format(dest_dir))
-    if result.failed:
-        return False
+    result = sudo("mkdir -p {}".format(dest_dir))
+    if ret_val and result.failed:
+        ret_val = False
     result = sudo("tar -xzf {} -C {}".format(remote_archive, dest_dir))
-    if result.failed:
-        return False
+    if ret_val and result.failed:
+        ret_val = False
     result = run("rm {}".format(remote_archive))
-    ret_val = result.succeeded
-    result = run("unlink /data/web_static/current")
+    if ret_val and result.failed:
+        ret_val = False
+    result = sudo("mv {}web_static/* {}".format(dest_dir, dest_dir))
+    if ret_val and result.failed:
+        ret_val = False
+    result = sudo("rm -rf {}web_static/".format(dest_dir))
+    if ret_val and result.failed:
+        ret_val = False
+    result = run("rm -rf /data/web_static/current")
+    if ret_val and result.failed:
+        ret_val = False
     if ret_val and result.failed:
         ret_val = False
     result = run("ln -s {} /data/web_static/current".format(dest_dir))
