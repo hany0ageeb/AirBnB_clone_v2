ede6ee784cbdb04163cd77cb06e34c5e1a1ecc5d
hany0ageeb <hany0ageeb@gmail.com>
2024-03-22 03:54:33 -0700

    Update City class - check for storage engine type

--- models/city.py
+++ models/city.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -7,8 +9,23 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column("state_id", ForeignKey("states.id"), nullable=False)
-    name = Column("name", String(128), nullable=False)
-    state = relationship('State', back_populates="cities")
-    places = relationship('Place', back_populates='city', cascade='all,delete')
+    if type(models.storage) is DBStorage:
+        __tablename__ = "cities"
+        state_id = Column(
+                "state_id",
+                ForeignKey("states.id"),
+                nullable=False)
+        name = Column(
+                "name",
+                String(128),
+                nullable=False)
+        state = relationship(
+                'State',
+                back_populates="cities")
+        places = relationship(
+                'Place',
+                back_populates='city',
+                cascade='all,delete')
+    else:
+        state_id = ""
+        name = ""
