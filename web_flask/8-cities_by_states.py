0d960e896ec8f98eefe7c71ad57db71705ff6607
hany0ageeb <hany0ageeb@gmail.com>
2024-03-22 10:53:50 -0700

    oops forget teardown storage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,5 +17,11 @@ def cities_by_states():
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_context(exce):
+    """teardown storage engine"""
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
